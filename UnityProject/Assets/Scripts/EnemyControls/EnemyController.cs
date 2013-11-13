a25c03f7cea28cc189b55b5a9efc1e71a89e2f85
Shezwick <shezwick@gmail.com>
2013-11-12 22:34:50 -0500

    Added Enemy Files
    Need some help adapting the scripts to check for the ground, as of now
    the test enemy just falls right through the floor

---
+++ UnityProject/Assets/Scripts/EnemyControls/EnemyController.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : Person {
+
+	//Enemy Handling
+	public float gravity = 20;
+	public float speed = 1;
+	public float counter = 75;
+//	public float walkSpeed = 8;
+//	public float runSpeed = 12;
+//	public float acceleration = 30;
+//	public float jumpHeight = 12;
+//	public float slideDecceleration = 10;
+	
+	
+	int distFromStart = 0;
+	
+	//System
+	private float animationSpeed;
+	private float currentSpeed;
+	private float targetSpeed;	
+	private Vector2 amountToMove;
+	
+	//States
+	private bool jumping;
+	private bool sliding;
+	private bool left;
+	
+	//Components
+	private EnemyPhysics enemyPhysics;
+
+	// Use this for initialization
+	void Start () {
+		enemyPhysics = GetComponent<EnemyPhysics>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		if(enemyPhysics.movementStopped)
+		{
+			//reset accel. when player hits wall
+			targetSpeed = 0;
+			currentSpeed = 0;
+			
+		}
+		
+		//scuttle left or right
+		if (left)
+		{
+			amountToMove.x = speed;
+			amountToMove.y -= gravity * Time.deltaTime;
+			enemyPhysics.Move(amountToMove * Time.deltaTime);
+		}
+		else
+		{
+			amountToMove.x = -(speed);
+			amountToMove.y -= gravity * Time.deltaTime;
+			enemyPhysics.Move(amountToMove * Time.deltaTime);
+		}
+		
+		//track movement
+		if (left)
+		{
+			distFromStart++;
+		}
+		else
+		{
+			distFromStart--;
+		}
+		
+		//change directions
+		if (distFromStart >= counter || distFromStart <= -(counter))
+			left = !left;
+		
+		
+	}
+	
+	//Increase n towards target by speed
+	private float IncrementTowards(float n, float target, float a) {
+		if (n == target)
+		{
+			return n;
+		}
+		else
+		{
+			float dir = Mathf.Sign(target - n); //must n be increased or decreased to get closer to target
+			n += a * Time.deltaTime * dir;
+			return (dir == Mathf.Sign(target - n))? n: target; //if n has now passed target then return target, otherwise return n
+					//(condition)? inside if: else
+		}
+	}//end IncrementTowards
+	
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.tag == "Player")
+		{
+				c.GetComponent<PlayerModel>().Damage(PlayerModel.FALL_DAMAGE);
+		}
+		else if (c.tag == "Jelly")
+		{
+			
+		}
+		else if (c.tag == "PeanutButter")
+		{
+			
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/EnemyControls/EnemyPhysics.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPhysics : MonoBehaviour {
+
+	public LayerMask collisionMask;
+	
+	private BoxCollider collider;
+	private Vector3 s; //size
+	private Vector3 c; //center
+	
+	private Vector3 originalSize;
+	private Vector3 originalCenter;
+	private float colliderScale;
+	
+	private int collisionDivisionsX = 3;
+	private int collisionDivisionsY = 10;
+	
+	private float skin = 0.005f;
+	
+	[HideInInspector]
+	public bool grounded;
+	
+	[HideInInspector]
+	public bool movementStopped;
+	
+	Ray ray;
+	RaycastHit hit;
+	
+	void Start()
+	{
+		collider = GetComponent<BoxCollider>();
+		colliderScale = transform.localScale.x;
+		
+		originalSize = collider.size;
+		originalCenter = collider.center;
+		SetCollider(originalSize, originalCenter);
+	}
+	
+	public void Move(Vector2 moveAmount)
+	{
+		float deltaX = moveAmount.x;
+		float deltaY = moveAmount.y;		
+		Vector2 p = transform.position; //enemy pos
+		
+
+		
+		//check collisions above and below
+		grounded = false;
+		for (int i = 0; i < collisionDivisionsX; i++)
+		{
+			float dir = Mathf.Sign (deltaY);
+			float x = (p.x + c.x - s.x/2) + s.x/(collisionDivisionsX-1) * i; // Left, center, and then right point of collider
+			float y = p.y + c.y + s.y/2 * dir; // Bottom of collider
+			
+			ray = new Ray(new Vector2(x, y), new Vector2(0, dir));
+			Debug.DrawRay(ray.origin, ray.direction);
+			
+			if (Physics.Raycast (ray, out hit, Mathf.Abs(deltaY) + skin, collisionMask))
+			{
+				// Get Distance between enemy and ground
+				float dst = Vector3.Distance (ray.origin, hit.point);
+				
+				// Stop enemy's downwards movement after coming within skin width of a collider
+				if (dst > skin)
+				{
+					deltaY = dst * dir - skin * dir;
+				}
+				else
+				{
+					deltaY = 0;
+				}
+				
+				grounded = true;
+				break;
+			}
+		}
+		
+		//check collisions left and right
+		movementStopped = false;
+		for (int i = 0; i < collisionDivisionsY; i++)
+		{
+			float dir = Mathf.Sign (deltaX);
+			float x = p.x + c.x + s.x/2 * dir;
+			float y = p.y + c.y - s.y/2 + s.y/(collisionDivisionsY-1) * i;
+			
+			ray = new Ray(new Vector2(x, y), new Vector2(dir, 0));
+			Debug.DrawRay(ray.origin, ray.direction);
+			
+			if (Physics.Raycast (ray, out hit, Mathf.Abs(deltaX) + skin, collisionMask))
+			{
+				// Get Distance between enemy and ground
+				float dst = Vector3.Distance (ray.origin, hit.point);
+				
+				// Stop enemy's downwards movement after coming within skin width of a collider
+				if (dst > skin)
+				{
+					deltaX = dst * dir - skin * dir;
+				}
+				else
+				{
+					deltaX = 0;
+				}
+				
+				movementStopped = true;
+				break;
+			}
+		}
+		
+		
+		//diagonal movement check
+		if (!grounded && !movementStopped)
+		{
+			Vector3 enemyDir = new Vector3(deltaX, deltaY);
+			Vector3 o = new Vector3(p.x + c.x + s.x/2 * Mathf.Sign (deltaX), p.y + c.y + s.y/2 * Mathf.Sign (deltaY));
+		
+			ray = new Ray (o, enemyDir.normalized);
+		
+			if (Physics.Raycast(ray, Mathf.Sqrt(deltaX * deltaX + deltaY * deltaY), collisionMask))
+			{
+				grounded = true;
+				deltaY = 0;
+			}
+		}
+		
+		Vector2 finalTransform = new Vector2(deltaX, deltaY);
+		
+		transform.Translate(finalTransform, Space.World);
+	}
+	
+	
+	public void SetCollider(Vector3 size, Vector3 center)
+	{
+		collider.size = size;
+		collider.center = center;
+		
+		s = size * colliderScale;
+		
+		c = center * colliderScale;
+	}
+	
+	public void ResetCollider()
+	{
+		SetCollider(originalSize, originalCenter);
+	}
+}
