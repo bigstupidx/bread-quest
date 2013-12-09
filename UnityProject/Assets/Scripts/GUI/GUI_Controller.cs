25218c3259731d2a8985404ee115c79d3aa86d8e
Shezwick <shezwick@gmail.com>
2013-12-09 17:32:10 -0500

    Final
    what i could do

---
+++ UnityProject/Assets/Scripts/GUI/GUI_Controller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI_Controller : MonoBehaviour {
+
+	PlayerModel play;
+	// Use this for initialization
+	void Start () {
+		play = GetComponent<PlayerModel>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
