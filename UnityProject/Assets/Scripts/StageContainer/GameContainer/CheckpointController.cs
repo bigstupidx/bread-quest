b3dc82c3ee8cdcb508a518e2d8195aa45de8138d
Lucas Oliveira David <lucasolivdavid@gmail.com>
2013-12-12 17:45:51 -0500

    Checkpoint animations implemented

--- UnityProject/Assets/Scripts/StageContainer/GameContainer/CheckpointController.cs
+++ UnityProject/Assets/Scripts/StageContainer/GameContainer/CheckpointController.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class CheckpointController : MonoBehaviour {
 
 	void OnTriggerEnter(Collider _c) {
+
+		// checkpoint was reached by the player
 		if (_c.CompareTag("Player")) {
+
+			GetComponent<Animator>().SetTrigger("reached");
 			Tools.GameController().SetPlayerPosition(transform.position);
 		}
 	}
