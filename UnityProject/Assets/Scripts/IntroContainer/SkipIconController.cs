638f3ce08c9897824d86ae266f076faac8a1ab2d
Lucas Oliveira David <lucasolivdavid@gmail.com>
2013-12-13 05:37:00 -0500

    door sprite fixed; skip-icon added at intro

---
+++ UnityProject/Assets/Scripts/IntroContainer/SkipIconController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkipIconController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.ScaleBy(gameObject, iTween.Hash("x", 1.1f, "y", 1.1f, "looptype", iTween.LoopType.pingPong, "easetype", iTween.EaseType.easeInOutSine));
+	}
+}
