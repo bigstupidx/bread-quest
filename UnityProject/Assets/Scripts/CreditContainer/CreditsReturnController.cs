67b2fbf6f59083b125db6c81e355af4f031eaa46
Lucas Oliveira David <lucasolivdavid@gmail.com>
2013-12-13 05:11:52 -0500

    Issue with camera at credits scene fixed

---
+++ UnityProject/Assets/Scripts/CreditContainer/CreditsReturnController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsReturnController : MonoBehaviour {
+
+	void OnTriggerEnter(Collider _c)
+	{
+		if ( _c.CompareTag("Player") )
+		{
+			_c.GetComponent<CharacterController>().enabled = false;
+			_c.GetComponent<Animator>().SetFloat("speed", 0);
+			StartCoroutine(ReturnToMainMenu());
+		}
+	}
+
+	IEnumerator ReturnToMainMenu ()
+	{
+		yield return new WaitForSeconds(1f);
+		Application.LoadLevel("main-menu");
+	}
+}
